42e3a813797c0590badee217cf81dde765db8bf9
Aaron Hartwig <aaronhartwig7@gmail.com>
2015-02-03 20:14:55 -0600

    fixes name binding

--- home/homeCtrl.js
+++ home/homeCtrl.js
@@ -2,10 +2,9 @@ angular
 	.module('handbook')
 	.controller('homeCtrl', ['$scope', function($scope) {
 			$scope.hasUsername = false;
-			$scope.username;
 
 			$('#app-header').on('click', '.tab', function(){
 				$('#app-header .tab').removeClass('active');
 				$(this).addClass('active');
 			});
-		}])
+		}]);
