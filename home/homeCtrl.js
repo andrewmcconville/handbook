b6a0c6dbfd6d21bb0b664529aa64c7d868e20268
Andrew McConville <andrew@andrewmcconville.com>
2015-02-07 17:43:43 -0600

    positions popups and highlight

--- home/homeCtrl.js
+++ home/homeCtrl.js
@@ -1,10 +1,10 @@
 angular
 	.module('handbook')
 	.controller('homeCtrl', ['$scope', function($scope) {
-			$scope.hasUsername = false;
+		$scope.hasUsername = false;
 
-			$('#app-header').on('click', '.tab', function(){
-				$('#app-header .tab').removeClass('active');
-				$(this).addClass('active');
-			});
-		}]);
+		$('#app-header').on('click', '.tab', function(){
+			$('#app-header .tab').removeClass('active');
+			$(this).addClass('active');
+		});
+	}]);
---
+++ part-1/understanding-your-project-or-assignment/understanding-other-projects/learn-it/2/understandingOtherProjectsLearn2.js
@@ -0,0 +1,6 @@
+angular
+	.module('handbook')
+	.controller('understandingOtherProjectsLearn2', ['$scope', function($scope) {
+		$scope.radioState = 0;
+		console.log($scope.radioState);
+	}]);
