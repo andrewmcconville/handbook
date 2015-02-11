db2fba58fe0ccc3acdb097dcb4c15ee2f9a4703c
Aaron Hartwig <aaronhartwig7@gmail.com>
2015-02-10 20:43:31 -0600

    wtf

--- shared/js/app.js
+++ shared/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('handbook', ['ui.router', 'ngAnimate']);
+var app = angular.module('handbook', ['ui.router', 'ngAnimate', 'ui.sortable']);
 
 /*
  * Directives
@@ -283,7 +283,38 @@ app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider,
 			})
 			.state('part-1.rhetoric-and-a-process-for-composing.try-it-1', {
 				url: '/try-it-1',
-				templateUrl: 'part-1/rhetoric-and-a-process-for-composing/try-it/1/1.html'
+				templateUrl: 'part-1/rhetoric-and-a-process-for-composing/try-it/1/1.html',
+				controller: function($scope) {
+					$scope.leftList = [{title:'Asking questions', class:'draggable drag-1'},
+						{title:'Drafting a paper', class:'draggable drag-2'},
+						{title:'Revising', class:'draggable drag-3'},
+						{title:'Understanding your project', class:'draggable drag-4'},
+						{title:'Shaping your project for others', class:'draggable drag-5'},
+						{title:'Polishing', class:'draggable drag-6'},
+						{title:'Getting feedback', class:'draggable drag-7'},
+						{title:'Getting started', class:'draggable drag-8'}];
+
+					$scope.rightList = [];
+
+					$scope.$watch('leftList', function(vals) {
+						console.log(vals);
+					});
+
+					$scope.$watch('rightList', function(vals) {
+						console.log(vals);
+					});
+
+					function createOptions () {
+						var options = {
+							placeholder: 'swag',
+							connectWith: 'item-container',
+							sort: function() {
+								console.log("lolk");
+							}
+						};
+						return options;
+					}
+				}
 			})
 		.state('part-1.understanding-your-project-or-assignment', {
 			url: '/understanding-your-project-or-assignment',
