dc533244aa7f3b4939779d65892242a04e6f1549
Andrew McConville <andrew@andrewmcconville.com>
2015-03-04 18:44:54 -0600

    moves directives out of app.js and into their own file

--- home/homeCtrl.js
+++ home/homeCtrl.js
@@ -3,6 +3,9 @@ angular
 	.controller('homeCtrl', ['$scope', function($scope) {
 		$scope.hasUsername = false;
 
+		/*
+		 * used to animate active main nav tab
+		 */
 		$('#app-header').on('click', '.tab', function(){
 			$('#app-header .tab').removeClass('active');
 			$(this).addClass('active');
